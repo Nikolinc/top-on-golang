3ba01225f09a2850afbdd5262e54a4105b8a4e92
Nikita Tsigulsky <tsigulskynikita@gmail.com>
2026-03-31 19:28:23 +0400

    fix print

--- internal/ui/showtasks.go
+++ internal/ui/showtasks.go
@@ -6,7 +6,7 @@ import (
 )
 
 func ShowTasks(tasks system.TasksState) {
-	fmt.Printf("Tasks: %4d total, %4d running, %4d sleeping, %4d stopped, %4d zombie /n",
-							tasks.Total, tasks.Running, tasks.Sleeping, tasks.Stopped, tasks.Zombie,
-						)
+	fmt.Printf("Tasks: %4d total, %4d running, %4d sleeping, %4d stopped, %4d zombie \n",
+		tasks.Total, tasks.Running, tasks.Sleeping, tasks.Stopped, tasks.Zombie,
+	)
 }
