a6c72fe352fcc8ffc3f0a257d9ed65fae67a47aa
Nikita Tsigulsky <tsigulskynikita@gmail.com>
2026-03-31 18:31:06 +0400

    change open file to readfile

--- internal/system/meminfo.go
+++ internal/system/meminfo.go
@@ -1,46 +1,40 @@
 package system
 
 import (
-	"io"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
-
 
 type MemStat struct {
 	Total, Used, Free, BuffCache, Available, SwapTotal, SwapFree, SwapUsed float64
 }
 
-
-func GetVolume(data []byte, count int) float64{
+func GetVolume(data []byte, count int) float64 {
 	line := strings.Split(string(data), "\n")[count]
 	fields := strings.Fields(line)[1:]
 	if len(fields) < 2 {
 		return 0
 	}
-	val,_ := strconv.ParseFloat(fields[0],64)
+	val, _ := strconv.ParseFloat(fields[0], 64)
 	return val
 }
 
 func MemInfo() MemStat {
-	file, _ := os.Open("/proc/meminfo")
-	defer file.Close()
-	data, _ := io.ReadAll(file)
+	data, _ := os.ReadFile("/proc/meminfo")
 
 	var total, used, free, buffcache, available float64
 	var swaptotal, swapfree, swapused float64
 
-
-	total = GetVolume(data, 0)/1024
-	free = GetVolume(data, 1)/1024
-	available = GetVolume(data, 2)/1024
+	total = GetVolume(data, 0) / 1024
+	free = GetVolume(data, 1) / 1024
+	available = GetVolume(data, 2) / 1024
 	used = total - available
 	buffcache = GetVolume(data, 3)/1024 + GetVolume(data, 4)/1024
 
-	swaptotal = GetVolume(data, 14)/1024
-	swapfree = GetVolume(data, 15)/1024
-	swapused =  swaptotal - swapfree
+	swaptotal = GetVolume(data, 14) / 1024
+	swapfree = GetVolume(data, 15) / 1024
+	swapused = swaptotal - swapfree
 
 	return MemStat{
 		Total: total, Used: used, Free: free, BuffCache: buffcache,
